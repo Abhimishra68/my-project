04fa96a50ed1b8fd030e3b134f4a2c0c8922223d
abvhishek <abhishekmishra08195@gmail.com>
2025-05-29 10:14:48 +0000

    Project

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Spring Boot Application...");
+		System.out.println("Spring Boot Application...");
 		SpringApplication.run(Main.class, args);
 	}
-
+//Abhishek opened github
 }
