bc2f6ee6f0f351e68aadbfa0244fce678faf34e9
abhishek <abhishekmishra08195@gmail.com>
2025-05-29 10:27:56 +0000

    abhishek

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Spring Boot Application...");
+		System.out.println("Abhishek");
 		SpringApplication.run(Main.class, args);
 	}
-//Abhishek opened github
+
 }
